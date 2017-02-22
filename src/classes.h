557a5046613ccd7feae5388a936d2fe6d02a286c
Andrew Gilbert <andrew.gilbert@cern.ch>
2017-02-22 17:17:03 +0100

    adding pdfs needed for debugging mssm hbb cards

--- src/classes.h
+++ src/classes.h
@@ -32,6 +32,8 @@
 #include "HiggsAnalysis/CombinedLimit/interface/RooMorphingPdf.h"
 #include "HiggsAnalysis/CombinedLimit/interface/RooParametricHist.h"
 #include "HiggsAnalysis/CombinedLimit/interface/GaussExp.h"
+#include "HiggsAnalysis/CombinedLimit/interface/RooDoubleGausExp.h"
+#include "HiggsAnalysis/CombinedLimit/interface/RooExtendNovosibirsk.h"
 
 namespace {
     struct dictionary {
